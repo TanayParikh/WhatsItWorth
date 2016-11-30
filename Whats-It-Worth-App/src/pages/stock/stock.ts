6ab7ac73372350c61c1d9dac01701438d05c2690
Tanay <tanay@webexposure.ca>
2016-11-29 21:34:42 -0500

    Added ability to find comparable product

--- Whats-It-Worth-App/src/pages/stock/stock.ts
+++ Whats-It-Worth-App/src/pages/stock/stock.ts
@@ -9,35 +9,109 @@ import { get } from 'http';
 })
 export class StockPage {
   stock: any;
+  products: any[];
+  product: JSON;
 
   constructor(public navCtrl: NavController, private navParams: NavParams) {
     this.stock = navParams.data.stock;
-    this.getStockInformation(this.stock.symbol);
+
+    console.log(this.stock);
+
+    this.setProducts();
+    this.getStockInformation(this.stock)
   }
 
   ionViewDidLoad() {
     console.log('Hello StockPage Page');
   }
 
-  getStockInformation(symbol){
-    const baseURL = 'http://query.yahooapis.com/v1/public/yql?format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env&q=select LastTradePriceOnly from yahoo.finance.quotes where symbol = ';
-    var queryURL = baseURL + '"' + symbol + '"';
+  setProducts() {
+    if(!window.localStorage.getItem('products')){
+      this.getAllProducts();
+    }
 
-    get({path: queryURL}, function (res) {
+    this.products = JSON.parse(window.localStorage.getItem('products'));
+    console.log("THE PRODUCTS ARE:");
+    console.log(this.products);
+  }
+
+  getStockInformation(stock){
+    // Sets up YQL request URI
+    const baseURL = 'http://query.yahooapis.com/v1/public/yql?format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env&q=select LastTradePriceOnly from yahoo.finance.quotes where symbol = ';
+    var queryURI = baseURL + '"' + stock.symbol + (stock.exchange == 'tse' ? '.TO' : '') +'"';
+
+    console.log(stock.exchange);
+    console.log(queryURI);
+
+    var that = this;
+
+    get({path: queryURI}, function (res) {
       var body = '';
 
       res.on('data', function(chunk){
          body += chunk;
       });
 
+      // Parses response and calls for processing
       res.on('end', function(){
         var parsed = JSON.parse(body.toString());
-        this.displayStats(parsed.query.results.quote.LastTradePriceOnly);
+        that.displayComparison(<number>parsed.query.results.quote.LastTradePriceOnly);
       });
     });
   }
 
-  displayStats(price: number) {
+  displayComparison(price: any) {
+    //var product : JSON;
+
+    if (!price) {
+      alert("Sorry, we were unable to retrieve information for that stock at this time. Please try again.");
+      return;
+
+    }
     
+    var count : number;
+
+    while (true) {
+      var index = Math.floor((Math.random() * this.products.length));
+
+      console.log("scanning: " + index);
+
+      if (this.products[index].price < price) {
+        this.product = this.products[index];
+        break;
+      }
+
+      if (++count > 50) break;
+    }
+
+    console.log(this.product);
+  }
+
+  getAllProducts() {
+    const baseURL = 'http://www.webexposure.ca/WhatsItWorth/api/api.php/products';
+
+    get(baseURL, function (res) {
+      var body = '';
+
+      res.on('data', function(chunk){
+         body += chunk;
+      });
+
+      // Parses response and calls for processing
+      res.on('end', function(){
+        var rawProducts = JSON.parse(body.toString()).products.records;
+        console.log("Records: ");
+        console.log(rawProducts);
+        //this.tempFix = true;
+        var products = new Array();
+
+        rawProducts.forEach(product => {
+          products.push({name: product[1], plural_name: product[2],
+            category: product[3], price: product[5], img: product[6]});
+        });
+
+        window.localStorage.setItem('products',JSON.stringify(products));
+      });
+    });
   }
 }
